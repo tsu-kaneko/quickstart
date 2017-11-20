7cd0ad67f61b8eacf29cdc7407bbdd662b99b3cd
tsu-kaneko <gamuuma@gmail.com>
2017-11-20 14:57:55 +0900

    ルートページとタスク追加ページの作成

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,37 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');  //resources/views/tasks.blade.phpが読み込まれる
+});
+
+Route::post('/task', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if($validator->fails()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //TODO: Add New Task
+});
+
+Route::delete('/task/{task}', function(Task $task) {
+    //
 });
